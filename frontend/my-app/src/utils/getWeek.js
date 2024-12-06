fe892281473b914c2b7dcbfd679187a4ce57ed15
Emilio Navea <emilio.navea@hotmail.com>
2024-12-05 22:05:49 -0300

    Eliminar todo el contenido de la rama main

--- frontend/my-app/src/utils/getWeek.js
+++
@@ -1,8 +0,0 @@
-// src/utils/getWeek.js
-export function getWeek(date) {
-    const startDate = new Date(date.getFullYear(), 0, 1);
-    const days = Math.floor((date - startDate) / (24 * 60 * 60 * 1000));
-    const weekNumber = Math.ceil((days + startDate.getDay() + 1) / 7);
-    return weekNumber;
-  }
-  
